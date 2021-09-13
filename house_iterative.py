d30cf736c8972f2aa3e0e3b0c580e28ca9d14156
Matt <mattm458@gmail.com>
2021-09-13 13:35:14 -0400

    first attempt

--- house_iterative.py
+++ house_iterative.py
@@ -1,1 +1,9 @@
+sqft = 0
 rooms = int(input("How many rooms are in your house? "))
+
+for i in range(rooms):
+    w = int(input("Width of room " + str(i+1) + ": "))
+    l = int(input("Length of room " + str(i+1) + ": "))
+    sqft = sqft + (w * l)
+
+print("Your home is " + str(sqft) + " square feet")
--- house_recursive.py
+++ house_recursive.py
@@ -1,5 +1,13 @@
+sqft = 0
 rooms = int(input("How many rooms are in your house? "))
 
 def square_feet(i, total_rooms, sqft):
-    # TODO: Write square_feet as a recursive function
-    pass
+    if i > total_rooms:
+        print("Your home is " + str(sqft) + " square feet")
+    else:
+        w = int(input("Width of room " + str(i) + ": "))
+        l = int(input("Length of room " + str(i) + ": "))
+        sqft = sqft + (w * l)
+        square_feet(i+1, total_rooms, sqft)
+
+square_feet(1, rooms, 0)
